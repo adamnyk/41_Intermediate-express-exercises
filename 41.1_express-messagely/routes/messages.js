cd37965d9b279f54eb7990df089b36cad4f821ae
Adam Pecan <adamnyk@gmail.com>
2023-04-07 20:25:11 -0600

    41.1 submit

--- 41.1_express-messagely/routes/messages.js
+++ 41.1_express-messagely/routes/messages.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const router = new express.Router();
+const Message = require("../models/message");
+const ExpressError = require("../expressError");
+const {ensureLoggedIn, ensureCorrectUser} = require("../middleware/auth")
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,6 +16,19 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', async (req, res, next) => {
+    try {
+        const message = await Message.get(req.params.id)
+        
+        if (req.user.username != message.from_user.username &&
+            req.user.username != message.to_user.username) {
+            throw new ExpressError("Unauthorized!", 401)
+        }
+        return res.json({message})
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 /** POST / - post message.
@@ -18,6 +37,17 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+
+router.post('/', ensureLoggedIn, async (req, res, next) => {
+    try {
+        req.body.from_username = req.user.username
+        const message = await Message.create(req.body)
+
+        return res.json({message})
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 /** POST/:id/read - mark message as read:
@@ -28,3 +58,19 @@
  *
  **/
 
+router.post('/:id/read', ensureLoggedIn, async (req, res, next) => {
+    try {
+    
+        req.body.from_username = req.user.username
+        const checkMessage = await Message.get(req.params.id)
+        if (checkMessage.to_user.username !== req.user.username) {
+            throw new ExpressError("Unauthorized!", 401)
+        }
+        const message = await Message.markRead(req.params.id)
+
+        return res.json({message})
+    } catch (err) {
+        next(err)
+    }
+})
+module.exports = router;
